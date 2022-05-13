48201a4561c484f70a64d0bfd945b243bd437e7b
Daniel Lazar <lazardani0112@gmail.com>
2022-05-14 00:02:35 +0200

    refactor: remove unnecessary parts & simplify app component

---
+++ src/app.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.style.css';
+
+function AppComponent() {
+  return (
+    <div>
+      <p>Hello React & Tauri!</p>
+    </div>
+  );
+}
+
+export default AppComponent;
